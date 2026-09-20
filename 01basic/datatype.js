79511c3c11141c0f59d5b10693d4025c3dc62252
24052016-droid <24052016@kiit.ac.in>
2026-09-20 16:17:28 +0000

    var data

---
+++ 01basic/datatype.js
@@ -0,0 +1,19 @@
+"use strict";
+console.log(3+3)//readabilty
+console.log("prajkta")
+let name="prajkta"
+let age=18
+let isLoggedIn=false
+let state=null
+//number >=2 to the power 53
+//bigint
+//string =>""
+//boolean=>true/false
+//null=> standlone value
+//undefined 
+//symbol=>unique
+
+
+
+
+//object
---
+++ 01basic/variable.js
@@ -0,0 +1,11 @@
+const accountId=144553
+let accountEmail="prajkta09@google.com"
+var accountPassword="12345"
+accountCity="jaipur"
+//accountId=2
+accountEmail="hc@email.com"
+accountPassword="212121"
+accountCity="beg";
+console.log(accountId);
+let a;
+console.table([accountId,a,accountPassword,accountEmail,accountCity])
