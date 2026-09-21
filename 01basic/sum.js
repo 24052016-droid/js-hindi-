20d6a6c0d8e6dee219ec7cd05474490c5c5e0822
24052016-droid <24052016@kiit.ac.in>
2026-09-21 15:01:12 +0000

    summary

---
+++ 01basic/sum.js
@@ -0,0 +1,23 @@
+ //[primitive data type]
+ // 7 type :string number boolean null undefined symbol*unique making of any *  big int **
+ 
+  const score =100
+  //type script const score:number =100
+  const scoreValue=100.3
+  const isloggedin=true
+  const id =Symbol("123") 
+  const anotherid=Symbol("123")
+  console.log(id===anotherid)
+//[refrence type non primitive datya type ]
+// arrays , objects ,functions
+//dyanmaically type language
+const heros=["spiderman","batman"];
+let myobj={
+    name:"prajkta ",//object 
+    age:21,
+}
+
+const myfun=function(){
+    console.log("helloworld")
+    
+}
