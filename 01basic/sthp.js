9b6e20ec257e2a58015c08ad6b207a1505d643df
24052016-droid <24052016@kiit.ac.in>
2026-09-21 15:41:17 +0000

    stack heap

---
+++ 01basic/sthp.js
@@ -0,0 +1,19 @@
+//stack: primitive copy   one one last oen then last second
+let a1="myvalue"
+let a2=a1
+a2="myvalue2"
+console.log(a1)
+console.log(a2)
+
+
+let userone={
+    email:"user12gmail.com",
+    upi:"u1@ybl"
+
+}
+let usertwo=userone
+usertwo.email="user@gmail.com"
+console.log(userone);
+console.log(usertwo);
+//heap:non primitive refernec original value is changed block form 
+
