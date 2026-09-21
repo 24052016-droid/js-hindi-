2cd22179252b05fb26c7274cd0946305a78d45d7
24052016-droid <24052016@kiit.ac.in>
2026-09-21 14:26:07 +0000

    ope

---
+++ 01basic/operation.js
@@ -0,0 +1,17 @@
+let value=3
+let negvalue=-value
+console.log(negvalue)
+//log.console(2+2)
+let str1="prajkta"
+let str2="chitranshi"
+console.log(str1+" "+str2)
+console.log("1"+2)
+console.log(1+"2")
+console.log(1+2+"2")
+console.log("2"+1+1)
+console.log(+true)//true+ is not possible
+let gamecounter=100
+++gamecounter
+console.log(gamecounter)
+gamecounter++;
+console.log(gamecounter)
