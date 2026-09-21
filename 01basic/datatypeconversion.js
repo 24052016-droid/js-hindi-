c5919c05d8536bc7f6eaf833de49eb49986d0fde
24052016-droid <24052016@kiit.ac.in>
2026-09-21 13:58:01 +0000

    conversion of data type

---
+++ 01basic/datatypeconversion.js
@@ -0,0 +1,14 @@
+let score ="33 a"
+console.log(typeof score)
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+//"33"=>33
+//"33ac"=> not in number NaN
+//true =>1;false=>0;
+let islogedIn= 1
+let BooleanislogedIn=Boolean(islogedIn)
+console.log(BooleanislogedIn)
+let someNumber =33
+let stringNUmber=String(someNumber)
+console.log( typeof stringNUmber)
