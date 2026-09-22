91d319a62d9977aee85409237c4a7bff5e804255
24052016-droid <24052016@kiit.ac.in>
2026-09-22 13:02:20 +0000

    string

---
+++ 01basic/string.js
@@ -0,0 +1,15 @@
+const name="prajkta"
+const repocount=50
+//console.log(name+repocount+" value")
+console.log(`hello my name is ${name} and my rep is ${repocount}`);
+const gamename= new String("prajktahc")
+console.log(gamename[0])
+console.log(gamename.length)
+console.log(gamename.toUpperCase())
+console.log(gamename.charAt(2))
+const newString=" prajkta "
+console.log(newString)
+console.log(newString.trim())
+const url="http://prajkta.com/prajkta%20chitranshi"
+console.log(url.replace("%20","-"))
+console.log(url.includes("prajkta"))
