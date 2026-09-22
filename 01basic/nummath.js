0b080b156e7b3bbedcb4893ddb492672ec954964
24052016-droid <24052016@kiit.ac.in>
2026-09-22 13:19:14 +0000

    num

---
+++ 01basic/nummath.js
@@ -0,0 +1,11 @@
+const score =400
+const balance =new Number(100)//strict
+console.log(score)
+console.log(balance)
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+const othernumber=23.896
+console.log(othernumber.toPrecision(3))
+const hundreds=1000000
+console.log(hundreds.toLocaleString())
+console.log(hundreds.toLocaleString('en-In'))
