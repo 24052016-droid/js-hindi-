840a450e0269c78117b4bab74532e91bd9b415e2
24052016-droid <24052016@kiit.ac.in>
2026-09-22 13:55:47 +0000

    math and date

---
+++ 01basic/math.js
@@ -0,0 +1,12 @@
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.floor(7.65))
+console.log(Math.ceil(4.56))
+console.log(Math.min(4,3,5,6))
+console.log(Math.max(4,3,5,6))
+console.log(Math.random())//range btw 0and 1 
+console.log(Math.floor(Math.random()*10)+1)
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
